767e1bb867b2d4828d59d8ed77be661c7c77a70a
Lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2019-04-01 03:34:40 +0530

    added simple input-output prog

---
+++ HardcoreCPP/0-Basics/1-input-output.cpp
@@ -0,0 +1,11 @@
+// read adn write first name on console
+#include "std_lib_facilities.h"
+
+int main()
+{
+	string first_name; // variable defination
+	cout<<"Enter your first name: "; // prompt
+	cin>>first_name;
+	cout<<"Hello,"<<first_name<<".\n";
+	return 0;
+}
