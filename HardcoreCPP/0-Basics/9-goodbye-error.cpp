5c21a6089cf1d9ae0fe8de1213226258c9bfb3e2
Lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2019-04-02 04:27:09 +0530

    added word count and error detec t

---
+++ HardcoreCPP/0-Basics/9-goodbye-error.cpp
@@ -0,0 +1,16 @@
+// program  that contians some error 
+// all error will be removed  wqhile debugging 
+
+#include "std_lib_facilities.h"
+// int Main()
+main()
+{
+	//STRING s = " Goodbye, cruel world!"
+	string s = " Goodbye, cruel world!";
+
+	//cOut<<S<<'\n';
+	//cbrt<<S<<'\n';
+	//cbrt<<s<<'\n';
+	cout<<s<<'\n';
+ 	//return 0;
+}
