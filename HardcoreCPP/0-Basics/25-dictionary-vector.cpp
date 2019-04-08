09e86c4a3b535df0b52e36fd87d3b9cde4509739
Lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2019-04-09 05:11:30 +0530

    ch 4 done

--- HardcoreCPP/0-Basics/25-dictionary-vector.cpp
+++ HardcoreCPP/0-Basics/25-dictionary-vector.cpp
@@ -5,8 +5,18 @@ int main()
 {
 	vector <string> words;
 
-	for(string temp; cin>>temp;)
+	for(string temp; cin>>temp;) // we use cin>>temp in condition of for loop
+								 // so that if the entered data is not integer
+								 // it will stop taking data and loop will exit
+	{
 		words.push_back(temp);
+	}
+
+	// We could have used while loop
+	// But we have to limit the scope of temp variable
+	// So in for loop the temp variable wont be available
+	// after for loop
+
 
 	cout<<"Number of words: "<<words.size()<<"\n";
 
