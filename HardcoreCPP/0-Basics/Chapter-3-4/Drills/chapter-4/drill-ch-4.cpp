943f4c57480806e7b7c600202c56db91bbfc1638
Lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2019-05-29 17:50:05 +0530

    done on pc

---
+++ HardcoreCPP/0-Basics/Chapter-3-4/Drills/chapter-4/drill-ch-4.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	
+	while(cin>>num1>>num2)
+	{
+		cout<<num1<<" "<<num2;
+	}
+	
+	
+
+	//cout<<char(num1)<<" "<<char(num2);
+	
+	return 0;
+}
