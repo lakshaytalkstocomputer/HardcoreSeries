6ac30f7008fcbd38dbb83a3a58b7364d2f363ebe
Lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2019-06-27 20:29:53 +0530

    started python

---
+++ HardCorePython/sqrt.py
@@ -0,0 +1,26 @@
+def sqrt(x):
+    def average(a, b):
+        return (a+b)/2
+    
+    def improve(guess, a):
+        return average(guess, a/guess)
+
+    def absol(c):
+        if (c >= 0):
+            return c
+        elif c < 0:
+            return -c
+    
+    def use(guess):
+        # print("guess: " + str(guess))
+        if(absol(guess*guess - x)) <= 0.0001:
+            # print("final guess: " + str(guess))
+            return guess
+        else:
+            return use(improve(guess, x))       
+        
+    return use(1)
+    
+print(sqrt(2))
+    
+    
