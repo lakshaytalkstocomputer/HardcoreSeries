0ba2111031d36a267c307d76895bd493e0098bf4
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-08-23 13:31:09 +0530

    [ADDED] guess the number- asking number from user

--- HardcoreRust/guessing_game/src/main.rs
+++ HardcoreRust/guessing_game/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+// Import as we say in python or include in C, C++
+//  input output from std library.
+use std::io;
+
+fn main(){
+    // print here is macro not a function, '!' at end
+    print!("Please enter a number: ");
+    println!();
+    // Create a variable to store the value entered by user.
+    //   Look String is not imported, this comes in something
+    //   that is known as "prelude" in rust.
+    let mut guess = String::new();
+
+
+    let output = io::stdin()
+                            .read_line(&mut guess)
+                            .expect("Error in reading!");
+
+
+    println!("you guessed {}", guess);
+    println!("OUt put was {}", output);
+
+    // Experimenting
+    // println!("Please enter a number: {} ",io::stdin()
+    //                         .read_line(&mut guess)
+    //                         .expect("Error in reading!"));
+    // println!("You entered {}", guess);
 }
+
