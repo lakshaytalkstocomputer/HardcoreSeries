e103d816fda3057791241f261457fad4d3322f01
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-08-31 00:00:35 +0530

    [ADDED] intiitlaizing variables

--- HardcoreRust/variables/src/main.rs
+++ HardcoreRust/variables/src/main.rs
@@ -1,3 +1,50 @@
 fn main() {
-    println!("Hello, world!");
+    /* Mutability adn Immutability */
+    let mut mutable_number = 9;
+    println!("The value of mutable_number is: {} ", mutable_number);
+
+    mutable_number = 27;
+    println!("The value of mutable_number has been changed to {} ", mutable_number);
+
+    mutable_number = 30;
+    println!("The  value of mutable_number has again been changed to  {}", mutable_number);
+
+    let immutable_number = 10;
+    println!("The Value of immutable_number: {}", immutable_number);
+
+    // this is an error
+    // // immutable_number = 11;
+
+    /* Constants */
+
+    const constant_number : u32 = 111;
+
+    // below is not allowed. Why? because const are immutable and cannot be changed to mutable!
+    // // const mut another_constant_number :u32 = 122;
+
+    // below is also not allowed. Why? Because not datatype!
+    // // const another_constant_numebr = 123;
+
+    /* Shadowing */
+
+    let some_number = 1;
+    println!("Value of some_number is : {}", some_number);
+
+    let some_number =2;
+    println!("Value of some_number has been changed to : {}", some_number);
+
+    // Why Shadowing?  Why not just use x = 5?
+    // // Since we cannot change the value of variable if mut is not mentioned
+    // //  Therefor e we need to mention mut.
+
+    let mut shadow_number = 1010;
+
+    // // this is not allowed! Why? Because it changes the type of the variable
+    // // shadow_number = "hello";
+
+
+
+
+
+
 }
