cc062ea9f05ecac3591f6fab868adb8784d72932
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-09-16 15:30:16 +0530

    [ADDED] boolean data type

--- HardcoreGO/hellogopher/data-types/datatypes.go
+++ HardcoreGO/hellogopher/data-types/datatypes.go
@@ -1,1 +1,23 @@
-package data_types
+package main
+
+
+import "fmt"
+
+var LightSwitchIsOn bool // Zero-Value of bool is false
+func main(){
+
+	printLightSwitchState()
+	toggleLightSwitch()
+	printLightSwitchState()
+	toggleLightSwitch()
+	printLightSwitchState()
+
+}
+
+func printLightSwitchState(){
+	fmt.Println("Light Switch is off: ", LightSwitchIsOn)
+}
+
+func toggleLightSwitch(){
+	LightSwitchIsOn = !LightSwitchIsOn
+}
