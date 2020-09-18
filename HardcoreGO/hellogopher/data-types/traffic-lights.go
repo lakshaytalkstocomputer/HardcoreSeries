d322cb7b71eee84fdd2e14293d3bef88f674f90a
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-09-18 12:03:22 +0530

    [ADDED] Modellin of traffic light signs

--- HardcoreGO/hellogopher/data-types/traffic-lights.go
+++ HardcoreGO/hellogopher/data-types/traffic-lights.go
@@ -1,1 +1,19 @@
 package main
+
+
+import "fmt"
+
+const (
+	// "_" is called blank identifier, it is used to ignore the value
+	_ = iota
+	TrafficLightStateRedLight
+	TrafficLightStateYellowLight
+	TrafficLightStateGreenLight
+)
+
+
+func main(){
+	fmt.Println("Red Light State Code: ",TrafficLightStateRedLight)
+	fmt.Println("Yellow Light State Code: ",TrafficLightStateYellowLight)
+	fmt.Println("Green Light State Code: ",TrafficLightStateGreenLight)
+}
