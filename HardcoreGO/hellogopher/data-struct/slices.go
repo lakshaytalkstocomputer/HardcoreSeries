242dc7a24ad4fcbf61621c1cae078103a20e146a
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-09-22 05:10:13 +0530

    [ADDED] slics

---
+++ HardcoreGO/hellogopher/data-struct/slices.go
@@ -0,0 +1,15 @@
+package main
+
+
+import "fmt"
+
+func main(){
+	/*
+	    * Slices can grow and shrink
+	    * Slices are references
+	    * make() to create slices
+	    * cap() Capacity function
+	    * append()
+	    *
+	 */
+}
