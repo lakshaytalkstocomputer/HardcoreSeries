549abd38f9e786e02fb202d1a76aa92063d0505e
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-09-25 09:47:09 +0530

    [ADDED] slices

--- HardcoreGO/hellogopher/data-struct/slices.go
+++ HardcoreGO/hellogopher/data-struct/slices.go
@@ -1,7 +1,22 @@
 package main
 
-
 import "fmt"
+
+func printArray(array []int32){
+	fmt.Println("Array : ", array)
+	fmt.Println("Length of Slice: ", len(array))
+	fmt.Println("Capacity of Slice: ", cap(array))
+}
+
+func populateArray(array []int32){
+	array[0] = 111
+	array[1] = 222
+	array[2] = 333
+	array[3] = 444
+	array[4] = 555
+
+}
+
 
 func main(){
 	/*
@@ -9,7 +24,21 @@ func main(){
 	    * Slices are references
 	    * make() to create slices
 	    * cap() Capacity function
-	    * append()
+	    * append() to put new element to slice
 	    *
 	 */
+	var someSlice []int32 = make([]int32, 5)
+	printArray(someSlice)
+
+	someSlice[0] = 1
+	printArray(someSlice)
+
+	populateArray(someSlice)
+	printArray(someSlice)
+
+	someSlice = append(someSlice, 10)
+	printArray(someSlice)
+
+	someSlice = append(someSlice, 11)
+	printArray(someSlice)
 }
