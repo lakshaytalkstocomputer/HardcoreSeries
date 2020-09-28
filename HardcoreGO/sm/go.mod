31d2799eaa6cf9dea3c93979a29c270c32facb13
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-09-28 09:26:03 +0530

    [ADDED] modules, socuialmedia post

--- HardcoreGO/sm/go.mod
+++ HardcoreGO/sm/go.mod
@@ -1,3 +1,5 @@
 module sm
 
 go 1.15
+
+require github.com/lakshaytalkstocomputer/HardcoreSeries/HardcoreGO/socialmedia v0.0.0-20200928031647-763de24529e1 // indirect
--- HardcoreGO/sm/sm.go
+++ HardcoreGO/sm/sm.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"github.com/lakshaytalkstocomputer/HardcoreSeries/HardcoreGO/socialmedia"
-	//"socialmedia"
 	)
 
 func main(){
+	myPost := socialmedia.NewPost(
+		"lakshaytalksto",
+		socialmedia.Moods["thrilled"],
+		"Go is awesome!",
+		"Check out the website",
+		"https://golang.org",
+		"",
+		"",
+		[]string{"go", "golang", "programming language"},
+		)
+
+	fmt.Printf("My Post: %+v\n", myPost)
 
 }
