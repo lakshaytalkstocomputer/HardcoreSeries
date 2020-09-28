e28aa69988b71e9ae2d5a5a6c08f6fbda3bfcefa
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-09-28 09:06:44 +0530

    [ADDED] hello module

---
+++ HardcoreGO/hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/lakshaytalkstocomputer/HardcoreSeries/HardcoreGO/hello
+
+go 1.15
---
+++ HardcoreGO/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lakshaytalkstocomputer/HardcoreSeries/HardcoreGO/hello/morestrings"
+)
+
+func main(){
+	fmt.Println("Hello world!")
+	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
+}
