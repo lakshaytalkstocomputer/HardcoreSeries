8045b638fcca4ca4715596f7b80afd223965f433
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-10-02 18:22:39 +0530

    [ADDED] intrumentation

--- HardcoreGO/stringsvc/main.go
+++ HardcoreGO/stringsvc/main.go
@@ -4,6 +4,9 @@ package main
 import (
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	"os"
 )
@@ -12,10 +15,34 @@ func main(){
 	// Creating an instance of logger
 	logger := log.NewLogfmtLogger(os.Stderr)
 
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:		"request_count",
+		Help:		"Number fo requests recieved.",
+	}, fieldKeys)
+
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:		"request_latency_microseconds",
+		Help:		"total duration of requests in microseconds.",
+	}, fieldKeys)
+
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "my_group",
+		Subsystem: "string_service",
+		Name:		"count_result",
+		Help:		"Result of each count method",
+	}, []string{})
+
 	// Create a  service
 	var svc StringService
 	svc = stringService{}
 	svc = loggingMiddleWare{logger: logger, next: svc}
+	svc = instrumentingMiddleware{requestCount:requestCount, requestLatency:requestLatency,
+		countResult: countResult, next: svc}
 
 	// Create Handler for the functions
 	uppercaseHandler := httptransport.NewServer(
@@ -32,6 +59,8 @@ func main(){
 
 	http.Handle("/uppercase",uppercaseHandler)
 	http.Handle("/count", countHandler)
+	http.Handle("/metrics", promhttp.Handler())
+	logger.Log("msg", "HTTP", "addr", ":8080")
 	logger.Log(http.ListenAndServe(":8080", nil))
 
 }
