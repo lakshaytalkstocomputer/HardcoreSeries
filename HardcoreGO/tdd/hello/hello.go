e17e9a3553dd52941ccaadd97cf99a3e767f81e4
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2020-10-08 14:06:41 +0530

    [ADDED] commment for clear deffination of somethings

--- HardcoreGO/tdd/hello/hello.go
+++ HardcoreGO/tdd/hello/hello.go
@@ -14,9 +14,7 @@ func main(){
 //return "Hello, World"
 //}
 //
-//func main(){
-//fmt.Println(Hello())
-//}
+
 
 
 // In above, we wrote function first, to know how to declare functions,
@@ -26,11 +24,13 @@ func main(){
 //  iterate on our software safely.
 
 // We should start by capturing the requirements in a test.
-//  Head over to hello2_test.go to check the test and requirement.
+//  Head over to hello_test.go to check the test and requirement.
 
 // When you retrospectively write tests there is a risk that your
 //  test may continue to pass even uf the code doesn't work as intended.
 package main
+
+import "fmt"
 
 // Constants should improve the performance of your application
 //  as it saves you creating the "Hello, " string instance every time
@@ -42,13 +42,17 @@ const englishHelloPrefix = "Hello, "
 // We write this function by listening to compiler
 //  On First run, it tells that it did not find any function named Hello2()
 //    Then we define a function named Hello2()
-//  On Second run, it tells us that Hello2 should have an argument of type string
+//  On Second run, it tells us that Hello should have an argument of type string
 //    Then we change the function to accept string and return string
 //  On third run, it tells us that the test that runs Hello2 fails,becuase it did not return the expected
 //    Then we change the logic of function
-func Hello2(name string) string{
+func Hello(name string) string{
 	if name == ""{
 		name = "World"
 	}
 	return englishHelloPrefix + name
+}
+
+func main(){
+	fmt.Println(Hello("Lakshay"))
 }
--- HardcoreGO/tdd/hello/hello_test.go
+++ HardcoreGO/tdd/hello/hello_test.go
@@ -2,10 +2,15 @@ package main
 
 import "testing"
 
-func TestHello2(t *testing.T){
+func TestHello(t *testing.T){
 	// Here we define the requirement of hello2 Function
-	//  Each requirement would be in  sub-test,
-	//  We can write sub-test using t
+	//  Each requirement would be in  sub-test, We can write sub-test using t.Run.
+	//  It is useful to group tests around a "thing" and then have subtests describing
+	//  different scenarios
+	// Refactoring is not just for production code.
+	// It is important that your tests are clear specifications of what the code needs to do.
+	// We can and should refactor the tests.
+
 
 	//  We first define the function which compares got and want.
 	//   This function helps us to avoid duplication and improves readability
@@ -23,14 +28,14 @@ func TestHello2(t *testing.T){
 
 	// We can define the requirement as one sub test
 	t.Run("saying hello to people", func(t *testing.T){
-		got := Hello2("Lakshay")
+		got := Hello("Lakshay")
 		want := "Hello, Lakshay"
 		assertCorrectMessage(t, got, want)
 	})
 
 	// We define other requirement as other subtest
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello2("")
+		got := Hello("")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
