645aca20e38c91fffcc1d224a0f5527d3985dd1d
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2020-10-08 14:05:09 +0530

    [ADDED] default for string

--- HardcoreGO/tdd/hello/hello.go
+++ HardcoreGO/tdd/hello/hello.go
@@ -1,7 +1,3 @@
-package main
-
-import "fmt"
-
 // This is not a good practice as the fmt.Println() is a side effect
 //  and string "Hello World" is our domain. So it is very important
 //  to separate these two out!
@@ -13,12 +9,46 @@ func main(){
 */
 
 // Above function should be written as :
-func Hello() string{
+//func Hello() string{
+//
+//return "Hello, World"
+//}
+//
+//func main(){
+//fmt.Println(Hello())
+//}
 
-	return "Hello, World"
+
+// In above, we wrote function first, to know how to declare functions,
+//  But from now we will write test first.
+
+// We Must have test of our function first in order to
+//  iterate on our software safely.
+
+// We should start by capturing the requirements in a test.
+//  Head over to hello2_test.go to check the test and requirement.
+
+// When you retrospectively write tests there is a risk that your
+//  test may continue to pass even uf the code doesn't work as intended.
+package main
+
+// Constants should improve the performance of your application
+//  as it saves you creating the "Hello, " string instance every time
+//  Hello2() is called.
+//
+// Refactor the constants
+const englishHelloPrefix = "Hello, "
+
+// We write this function by listening to compiler
+//  On First run, it tells that it did not find any function named Hello2()
+//    Then we define a function named Hello2()
+//  On Second run, it tells us that Hello2 should have an argument of type string
+//    Then we change the function to accept string and return string
+//  On third run, it tells us that the test that runs Hello2 fails,becuase it did not return the expected
+//    Then we change the logic of function
+func Hello2(name string) string{
+	if name == ""{
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
-
-func main(){
-	fmt.Println(Hello())
-}
-
--- HardcoreGO/tdd/hello/hello_test.go
+++ HardcoreGO/tdd/hello/hello_test.go
@@ -2,20 +2,36 @@ package main
 
 import "testing"
 
+func TestHello2(t *testing.T){
+	// Here we define the requirement of hello2 Function
+	//  Each requirement would be in  sub-test,
+	//  We can write sub-test using t
 
-// Writing a test is just like writing a function, with a few rules
-//  * It needs to be in file with a name like xxx_test.go
-//  * The test function start with the word Test
-//  * The test function takes only one argument only t *testing.T
-//
-// t of type *Testing.T is out "hook" into testing framework
-//  so we can do things like t.Fail() when out tests fails.
-func TestHello(t *testing.T){
-	got := Hello()
-	want := "Hello, World"
-
-	if got != want {
-		// This will print out the message and fail the test.
-		t.Errorf("got %q want %q", got, want)
+	//  We first define the function which compares got and want.
+	//   This function helps us to avoid duplication and improves readability
+	//   of our tests.
+	//
+	//  t.Helper() is needed to tell the test suite that this method is a helper.
+	//    By doing this when it fails the line number reported will be in our function
+	//    call rather than inside our test helper
+	assertCorrectMessage := func(t *testing.T, got string, want string){
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got ,want)
+		}
 	}
+
+	// We can define the requirement as one sub test
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello2("Lakshay")
+		want := "Hello, Lakshay"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// We define other requirement as other subtest
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello2("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
