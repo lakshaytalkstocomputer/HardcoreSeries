870fd34304fc151e71116b2a5c273a494dd3cbdc
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-10-09 22:23:36 +0530

    [ADDED] Area of rectangle

--- HardcoreGO/tdd/geometry/geometry.go
+++ HardcoreGO/tdd/geometry/geometry.go
@@ -1,6 +1,9 @@
 package geometry
 
-
 func Perimeter(height float64, width float64) float64{
 	return (2) * (height + width)
+}
+
+func Area(height float64, width float64) float64{
+	return height * width
 }
--- HardcoreGO/tdd/geometry/geometry_test.go
+++ HardcoreGO/tdd/geometry/geometry_test.go
@@ -7,7 +7,17 @@ func TestPerimeter(t *testing.T){
 	expected 	:= 40.0
 
 	if got != expected{
-		t.Errorf("Got %0.2f Expected %.2f", got, expected)
+		t.Errorf("Got %.2f Expected %.2f", got, expected)
 	}
 
+}
+
+
+func TestArea(t *testing.T){
+	got			:= Area(10.0, 10.0)
+	expected	:= 100.0
+
+	if got != expected{
+		t.Errorf("Got %.2f but expected %.2f", got, expected)
+	}
 }
