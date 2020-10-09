37f526592f5fd78931504b6586aa64e1e551c966
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-10-09 22:31:04 +0530

    [REFACTORED] test of are and permiter to include sub test

--- HardcoreGO/tdd/geometry/geometry_test.go
+++ HardcoreGO/tdd/geometry/geometry_test.go
@@ -3,24 +3,30 @@ package geometry
 import "testing"
 
 func TestPerimeter(t *testing.T){
+	t.Run("testing perimeter of rectangle", func(t *testing.T) {
+		rect 		:= Rectangle{10.0, 10.0}
+		got 		:= Perimeter(rect)
+		expected 	:= 40.0
 
-	rect 		:= Rectangle{10.0, 10.0}
-	got 		:= Perimeter(rect)
-	expected 	:= 40.0
-
-	if got != expected{
-		t.Errorf("Got %.2f Expected %.2f", got, expected)
-	}
+		if got != expected{
+			t.Errorf("Got %.2f Expected %.2f", got, expected)
+		}
+	})
 
 }
 
 
 func TestArea(t *testing.T){
-	rect 		:= Rectangle{10.0, 10.0}
-	got 		:= Area(rect)
-	expected	:= 100.0
 
-	if got != expected{
-		t.Errorf("Got %.2f but expected %.2f", got, expected)
-	}
+	t.Run("testing area of rectangle", func(t *testing.T) {
+		rect 		:= Rectangle{10.0, 10.0}
+		got 		:= Area(rect)
+		expected	:= 100.0
+
+		if got != expected{
+			t.Errorf("Got %.2f but expected %.2f", got, expected)
+		}
+	})
+
+
 }
