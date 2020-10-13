3b18a84761548e0d2beee51fa5cf0c17873db689
laskhaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-10-13 06:24:00 +0530

    [REFACTORED]

--- HardcoreGO/tdd/reflected/reflected.go
+++ HardcoreGO/tdd/reflected/reflected.go
@@ -6,28 +6,25 @@ func Walk(x interface{}, fn func(input string) ){
 
 	val := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
-
+	walkValue := func(value reflect.Value){
+		Walk(value.Interface(), fn)
+	}
 
 	switch val.Kind(){
-	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
-	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
 	case reflect.String:
 		fn(val.String())
 	case reflect.Map:
-		for _, key:= range val.MapKeys(){
-			Walk(val.MapIndex(key).Interface(), fn)
+		for _, key := range val.MapKeys(){
+			walkValue(val.MapIndex(key))
 		}
-
-	}
-
-	for i:=0; i<numberOfValues; i++{
-		Walk(getField(i).Interface(), fn)
+	case reflect.Struct:
+		for i:=0; i<val.NumField(); i++{
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i:=0; i<val.Len(); i++{
+			walkValue(val.Index(i))
+		}
 	}
 
 }
