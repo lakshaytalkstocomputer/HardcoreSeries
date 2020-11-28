663198f0a3578ff8c5cd39ea23559b7664ce9781
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2020-11-28 21:09:32 +0530

    [ADDED] event loop phases

---
+++ HardCoreJS/Node/event-loop-phases-3.js
@@ -0,0 +1,18 @@
+setImmediate(() => console.log(1));
+console.log(2);
+
+Promise.resolve().then(() => {
+    console.log(11);
+    setTimeout(() => {
+            setImmediate(() => console.log(3));
+            console.log(4);
+
+            Promise.resolve().then(() => setImmediate(()=> {
+                    setImmediate(() => console.log(5));
+                    console.log(6);
+                    Promise.resolve().then(() => {
+                                setImmediate(() => console.log(7));
+                                console.log(8);
+                            });
+                        })); 
+                    })});
