b6ad2fad83f98728225768a0ad25960f2ad44479
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2020-11-28 14:58:42 +0530

    [ADDED] node js event loop demo

---
+++ HardCoreJS/Node/mult-stack-js.js
@@ -0,0 +1,12 @@
+setTimeout(() => console.log('A'), 0 );
+console.log("B");
+setTimeout(() => console.log('C'), 100 );
+setTimeout(() => console.log('D'), 0 );
+
+let i = 0;
+while(i < 1_000_000_000) {
+    let ignore = Math.sqrt(i);
+    i++
+}
+
+console.log("E");
