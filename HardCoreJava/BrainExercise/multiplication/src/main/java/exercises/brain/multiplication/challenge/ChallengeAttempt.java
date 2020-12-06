0e81f2223295065c81ccb7b614e6df760382ffe5
lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-12-06 23:12:46 +0530

    [ADDED] Challenge Attempt

--- HardCoreJava/BrainExercise/multiplication/src/main/java/exercises/brain/multiplication/challenge/ChallengeAttempt.java
+++ HardCoreJava/BrainExercise/multiplication/src/main/java/exercises/brain/multiplication/challenge/ChallengeAttempt.java
@@ -1,4 +1,22 @@
 package exercises.brain.multiplication.challenge;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ *  This class identifies an attempt from a User to solve a Challenge.
+ */
+@EqualsAndHashCode
+@Getter
+@ToString
+@AllArgsConstructor
 public class ChallengeAttempt {
+    private Long id;
+    private Long userId;
+    private int factorA;
+    private int factorB;
+    private int resultAttempt;
+    private boolean correct;
 }
