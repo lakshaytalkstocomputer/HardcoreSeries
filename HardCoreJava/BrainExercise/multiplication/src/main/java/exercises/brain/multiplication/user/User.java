383a0a9b9a0c53c50d3b433b087712dce7a93c13
lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-12-06 23:09:57 +0530

    [ADDED] User class

--- HardCoreJava/BrainExercise/multiplication/src/main/java/exercises/brain/multiplication/user/User.java
+++ HardCoreJava/BrainExercise/multiplication/src/main/java/exercises/brain/multiplication/user/User.java
@@ -1,4 +1,18 @@
 package exercises.brain.multiplication.user;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * This class will represent the User  in the application and store the information about them.
+ */
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
 public class User {
+    private String alias;
+    private long id;
 }
