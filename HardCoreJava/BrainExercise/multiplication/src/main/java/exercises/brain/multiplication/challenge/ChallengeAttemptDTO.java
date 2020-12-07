7b13df5fd515293a31fcf7507500e0c6b257db96
lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-12-07 22:49:40 +0530

    [ADDED] ChallengeAttempt post and test

--- HardCoreJava/BrainExercise/multiplication/src/main/java/exercises/brain/multiplication/challenge/ChallengeAttemptDTO.java
+++ HardCoreJava/BrainExercise/multiplication/src/main/java/exercises/brain/multiplication/challenge/ChallengeAttemptDTO.java
@@ -2,12 +2,20 @@ package exercises.brain.multiplication.challenge;
 
 import lombok.Value;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 /**
  * Attempt coming form the user.
  */
 @Value
 public class ChallengeAttemptDTO {
+    @Positive(message = "this neeeds to be positive mate!")
     int guess;
+    @Min(1) @Max(99)
     int factorA, factorB;
+    @NotBlank
     String userAlias;
 }
