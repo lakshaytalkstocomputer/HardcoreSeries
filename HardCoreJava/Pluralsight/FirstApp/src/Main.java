444807be78425a67e34dff2d9de8bd78f54bc9e7
lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-12-26 13:03:05 +0530

    [ADDED] First Java programming

---
+++ HardCoreJava/Pluralsight/FirstApp/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+    // Does not run if i create a package like com.example
+    // Run in command line by using - java Main - in /out/production/
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("First Line from App");
+        System.out.println("Second Line from App");
+        System.out.println("Third line from App");
+    }
+}
