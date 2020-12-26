8280e18859923f7c6ffaa07627abb8fa43e8b1e3
lakshaytalkstocomputer <lakshaysagarrana@gmail.com>
2020-12-26 13:10:21 +0530

    [ADDED] Leacture ntoes

--- HardCoreJava/Pluralsight/FirstApp/src/Main.java
+++ HardCoreJava/Pluralsight/FirstApp/src/Main.java
@@ -1,7 +1,19 @@
+/**
+ *   JRE - Java Runtime Environment
+ *         This enables java code to execute.
+ *         Since Java code is a cross platform, we need an environment to run the same bytecode to run on different
+ *              architectures.
+ *   JDK - Java Development Kit
+ *         This provides to tool to create Java apps
+ *
+ *   IDE -> JDK -> JavaApp ->  JRE -> OS
+ *
+ *   User just need to install JRE for their platform.
+ */
+
+
 public class Main {
-
-
-    // Does not run if i create a package like com.example
+     // Does not run if i create a package like com.example
     // Run in command line by using - java Main - in /out/production/
     public static void main(String[] args) {
 	// write your code here
